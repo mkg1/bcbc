3afc952d9351909f96c8501a10895e55b805838e
mkg1 <gehring.mg@gmail.com>
2019-02-06 16:59:32 -0500

    Correct foreign key

--- books/models.py
+++ books/models.py
@@ -8,7 +8,6 @@ class Book(models.Model):
     year = models.IntegerField()
     description = models.TextField(blank = True)
     status = models.CharField(choices = STATUS, max_length = 1)
-    review = models.ForeignKey(Review)
 
 class Review(models.Model):
     REVIEW_TYPE = [(1, 'Member'), (2, 'Online')]
@@ -16,3 +15,4 @@ class Review(models.Model):
     rating = models.IntegerField()
     comments = models.TextField(blank = True)
     review_type = models.CharField(choices = REVIEW_TYPE, max_length = 1)
+    book = models.ForeignKey(Book)
